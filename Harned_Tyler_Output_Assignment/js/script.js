00b93b7915cb51f57eb0f620268f96bab8d4e624
Tyler Harned <trharned@fullsail.edu>
2015-07-09 22:38:06 -0500

    Base Created!
    I’ve created the base for the Output Assignment. This includes setting
    up the javascript files, the html file, and the base css files. Apart
    from that I went ahead and created all the variables.

---
+++ Harned_Tyler_Output_Assignment/js/script.js
@@ -0,0 +1,22 @@
+// Tyler Harned
+// Full Sail University - SDI_01
+// Output Assignment
+// July 9th, 2015
+
+// Different Types of variables
+// var age = 38; Number Variable
+// var isStudent = true; // Boolean Variable (True or false variable)
+// var firstName = "Tyler"; // String Variable
+
+var name = "Tyler"; // Tell everyone my name!
+var age = 21; // Tell everyone my age as well!
+var employed = true; // Tell people that I am currently employed.
+var jobOccupation = "Counter Sales"; // Discuss my job title.
+var jobName = "Napa Auto Parts"; // Where I work at
+var hobbies = "Gaming, Learning Code, and Developing Ideas!"; // List my hobbies! (Note: I forgot how to create a list.)
+
+alert("Hell there!\nLet me tell you a little about myself in the console!\nPlease make sure you can view the console."); // Tell everyone what I will be doing and get them ready.
+console.log("Hello world! My name is "+name+" and I am "+age+" years old. It is "+employed+" that I am currently employed at "+jobName+". What I do at "+jobName+" is "+jobOccupation+"."); // Combine all the things! This is where I discuss who I am, how old I am, what I do. Apart from that, I will discuss my hobby.
+console.log("Well, that is it about me! One last thing before I go; My hobbies include "+hobbies+" See you later!"); // List my hobbies and say goodbye.
+
+// Hope you enjoyed! Really had fun adding a lot of strings together. Also, forgot about the /n to make a new line. That's always handy. ;)
