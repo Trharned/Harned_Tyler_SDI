cf8649cf7670ff92f0ab3284eb76156e9d0652db
Tyler Harned <trharned@fullsail.edu>
2015-07-09 22:21:46 -0500

    Learning Session // Defining Variables
    Learned about defining variables.

---
+++ Values_and_Variables/Defining_Variables/js/script.js
@@ -0,0 +1,7 @@
+// Javascript --> Variables
+
+var age; // declaration
+age = 21; // definition
+//storage container = value
+
+var yearOfBirth = 1993; // declaring and defining on the same line! woo-hoo!
