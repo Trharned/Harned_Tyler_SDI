c62631f8548537536b993ed92c7b9495a9abbce0
Tyler Harned <trharned@fullsail.edu>
2015-07-09 22:13:14 -0500

    Console.log() and alert(); test
    Learned about console.log’s and alerts. Learned how to make them show
    up on the webpage.

---
+++ Values_and_Variables/console.log_and_alert/js/code.js
@@ -0,0 +1,13 @@
+// Tyler Harned
+// Full Sail Univeristy - SDI 01
+// Using Console().log and alert();
+// July 9th, 2015
+
+var yearBorn = prompt("Please type the year you where born: ");
+
+/*
+console.log("Hello World!");
+console.log(yearBorn);
+    */
+
+alert("Hello World! You where born in "+yearBorn+".");
