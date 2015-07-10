92a1da9910386eefe5796bc1b2b4481ff4e4345c
Tyler Harned <trharned@fullsail.edu>
2015-07-09 22:45:12 -0500

    Changes to alerts/console.logs
    I’ve changed how the alerts are showing and what’s logged into the
    console. New information is posted in the alert and all results are
    posted in the console.log()’s.

--- Harned_Tyler_Output_Assignment/js/script.js
+++ Harned_Tyler_Output_Assignment/js/script.js
@@ -8,6 +8,9 @@
 // var isStudent = true; // Boolean Variable (True or false variable)
 // var firstName = "Tyler"; // String Variable
 
+
+// Create variables and use them to tell everyone my name!
+
 var name = "Tyler"; // Tell everyone my name!
 var age = 21; // Tell everyone my age as well!
 var employed = true; // Tell people that I am currently employed.
@@ -15,8 +18,21 @@ var jobOccupation = "Counter Sales"; // Discuss my job title.
 var jobName = "Napa Auto Parts"; // Where I work at
 var hobbies = "Gaming, Learning Code, and Developing Ideas!"; // List my hobbies! (Note: I forgot how to create a list.)
 
-alert("Hell there!\nLet me tell you a little about myself in the console!\nPlease make sure you can view the console."); // Tell everyone what I will be doing and get them ready.
+// Greet the user!
+var greet = "Welcome! Let me show you who I am."; // Create a greet variable to include a message.
+alert(greet); // Make a pop-up to the user.
+console.log(greet); // log the results in the console.
+
+// Alert the user my name and age.
+alert("My name is "+name+". I am currently "+age+" years old."); // Show the user my name and age.
+console.log("The creator's name is "+name+" and is "+age+" years old."); // Log the results in the console.
+
+// Alert the user my job information.
+alert("It's "+employed+" that I am currently employed.\n\nI currently do "+jobOccupation+" at "+jobName+".");// Alert the user some job information
+console.log("The creator is employed: "+employed+".\nThe user does "+jobOccupation+" at "+jobName+"."); // Log the results of my occupation
+
+/*alert("Hell there!\nLet me tell you a little about myself in the console!\nPlease make sure you can view the console."); // Tell everyone what I will be doing and get them ready.
 console.log("Hello world! My name is "+name+" and I am "+age+" years old. It is "+employed+" that I am currently employed at "+jobName+". What I do at "+jobName+" is "+jobOccupation+"."); // Combine all the things! This is where I discuss who I am, how old I am, what I do. Apart from that, I will discuss my hobby.
-console.log("Well, that is it about me! One last thing before I go; My hobbies include "+hobbies+" See you later!"); // List my hobbies and say goodbye.
+console.log("Well, that is it about me! One last thing before I go; My hobbies include "+hobbies+" See you later!"); // List my hobbies and say goodbye.*/
 
 // Hope you enjoyed! Really had fun adding a lot of strings together. Also, forgot about the /n to make a new line. That's always handy. ;)
