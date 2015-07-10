ce2745974f47948d3da5a6bca78c1e8cd8c9a06e
Tyler Harned <trharned@fullsail.edu>
2015-07-09 22:17:38 -0500

    Variables // Learning JS
    Learning JavaScript // Learning about variables and what they are and
    how to properly name them.

---
+++ Values_and_Variables/Declaring_Variables/js/script.js
@@ -0,0 +1,9 @@
+// Javascript
+
+var age;
+
+// Use camelCasing
+// don't use special characters %!@%
+// Use _'s, letters numbers
+// Begin with lowercase letters
+// Names should be relevant and meaningful
