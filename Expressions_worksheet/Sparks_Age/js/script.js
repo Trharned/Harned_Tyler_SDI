a88d7be16065ab52814dc75d3e35ee2ad8970a68
Tyler Harned <trharned@fullsail.edu>
2015-07-11 19:42:14 -0500

    Sparky's Age Calculator
    Setup the base files, created the basic variables, and printed these to
    the console.

---
+++ Expressions_worksheet/Sparks_Age/js/script.js
@@ -0,0 +1,22 @@
+/*
+Tyler Harned
+Full Sail University
+SDI 01
+Expressions Worksheet - Sparky's Age Calculator
+July 11th, 2015
+ */
+
+// Create calculator to determine Sparky's age.
+
+// Create a variable to hold Sparky's age
+var sparkysAge = prompt("What is Sparky's age in human years?");
+// Print the result to the console
+console.log("Sparky's age is "+sparkysAge+" in human years.");
+
+// Create the calculation to determine his age in dog years
+var dogYears = sparkysAge * 7;
+
+// Let the user know how old sparky is in dog years.
+alert("Sparky is "+sparkysAge+" humans years old which is  "+dogYears+" in dog years.");
+// Print the final result to the console.
+console.log("Sparky is "+sparkysAge+" humans years old which is "+dogYears+" in dog years.");
