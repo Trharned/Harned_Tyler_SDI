46ad8c6c79c08ed7582acd99125e9f953508cf45
Tyler Harned <trharned@fullsail.edu>
2015-07-11 19:56:46 -0500

    Slice of Pie I Setup
    Created the basic files for Slice of Pie part I. This includes setting
    up the html file, js file, and the directories.

---
+++ Expressions_worksheet/Slice of Pie I/js/script.js
@@ -0,0 +1,11 @@
+/*
+ Tyler Harned
+ Full Sail University
+ SDI 01
+ Expressions Worksheet - Slice of Pie I
+ July 11th, 2015
+ */
+
+// Create a calculator to determine how many slices of pie each person will get at a party
+
+//Create the variable's for each parts needed
