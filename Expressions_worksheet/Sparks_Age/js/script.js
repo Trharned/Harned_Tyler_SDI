38af222727977f604e1614624f12ad01afc13165
Tyler Harned <trharned@fullsail.edu>
2015-07-11 19:43:02 -0500

    Sparky's age calculator changes
    Cleaned up the code by making it easier to read. This included spacing
    between each comment and line of code.

--- Expressions_worksheet/Sparks_Age/js/script.js
+++ Expressions_worksheet/Sparks_Age/js/script.js
@@ -10,6 +10,7 @@ July 11th, 2015
 
 // Create a variable to hold Sparky's age
 var sparkysAge = prompt("What is Sparky's age in human years?");
+
 // Print the result to the console
 console.log("Sparky's age is "+sparkysAge+" in human years.");
 
@@ -18,5 +19,6 @@ var dogYears = sparkysAge * 7;
 
 // Let the user know how old sparky is in dog years.
 alert("Sparky is "+sparkysAge+" humans years old which is  "+dogYears+" in dog years.");
+
 // Print the final result to the console.
 console.log("Sparky is "+sparkysAge+" humans years old which is "+dogYears+" in dog years.");
