2bbb6439e248a608de34ed99bf1f74b54cd7b7c0
Tyler Harned <trharned@fullsail.edu>
2015-07-13 13:48:34 -0500

    Created Website Directory & Finished Assignment Part 1
    Created a personal website directory for later, and completed the Pie I
    part of the assignment.

--- Expressions_worksheet/Slice of Pie I/js/script.js
+++ Expressions_worksheet/Slice of Pie I/js/script.js
@@ -9,3 +9,26 @@
 // Create a calculator to determine how many slices of pie each person will get at a party
 
 //Create the variable's for each parts needed
+// Number of slices per pizza
+// Number of people at the party
+// Number of pizzas ordered.
+
+// Create a variable to hold how many slices there is in a pizza
+var slicesPer = prompt("How many slices are in a pizza?");
+// Log that into the console.
+console.log("Each pizza holds "+slicesPer+" slices of pizza.");
+
+// Create a variable to hold how many people are at the party.
+var totalPeople = prompt("How many people are at the party?");
+// Log that into the console.
+console.log("There are a total of "+totalPeople+" at the party.");
+
+// Create a variable to hold how many pizzas where ordered at the party.
+var totalPizzas = prompt("How many pizzas total where ordered?");
+// Log that into the console.
+console.log("There where a total of "+totalPizzas+" ordered.");
+
+// Create the calculations to determine the results.
+var results = slicesPer * totalPizzas / totalPeople;
+// Log that into the console. 
+console.log("Each person ate "+results+" slices of pizza at the party.");
---
+++ website/js/script.js
@@ -0,0 +1,13 @@
+/*
+Tyler Harned
+
+Website JavaScript file
+
+This is for testing purposes, anything learned during my
+courses at Full Sail University will go towards finishing
+and creating my very own personal website. Also new ideas
+will also be tested to see how they function.
+ */
+
+alert("Hello World!");
+console.log("Hello World! This is a message.\nTesting to see if connected.");
