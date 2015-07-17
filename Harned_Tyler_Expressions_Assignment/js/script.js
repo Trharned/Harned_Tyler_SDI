16bc8431027977d4a2a0540f5975f90cb12e3b2b
Tyler Harned <trharned@fullsail.edu>
2015-07-16 21:09:04 -0500

    Created Variables
    Created the variables for everything that will be used.

--- Harned_Tyler_Expressions_Assignment/js/script.js
+++ Harned_Tyler_Expressions_Assignment/js/script.js
@@ -7,3 +7,15 @@
  */
 
 // Create a calculator that determines how many treats a user's pet eats in weeks, months, or years.
+
+// VARIABLES
+var myName; // Create a variable for the user's first name.
+var myAnimal; // Create a variable to determine if cat or dog
+var dailyTreats; // Used to determine how many treats the user's cat/dog gets a day
+var testMethod; // Will be used to test for either weekly, monthly, or yearly tests
+var weeklyResults; // Used for the final results of weekly
+var monthlyResults; // used for the final results of monthly
+var yearlyResults; // Used for the final results of yearly.
+var weeklyEndResult; // Used for the weekly calculations
+var monthlyEndResults; // Used for the monthly calculations
+var yearlyEndResults; // Used for the yearly calculations
