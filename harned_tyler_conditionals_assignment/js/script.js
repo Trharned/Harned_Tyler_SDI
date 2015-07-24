4ff55af11af027e4c1ffa9695e289409dd1b7ee5
Tyler Harned <trharned@fullsail.edu>
2015-07-23 22:25:20 -0500

    Added Comments
    Added more comments to ensure that the code was organized and neat.

--- harned_tyler_conditionals_assignment/js/script.js
+++ harned_tyler_conditionals_assignment/js/script.js
@@ -8,9 +8,9 @@
 // THE VARIABLES
 var myName = "Tyler Harned";                   // Created A Name Variable (STRING)
 var myAge = prompt("Please enter your age: "); // Created A Age Variable
-var drinker = "False";                // Created A Drinker Variables (Yes or No Question) (ESCAPE VARIABLE)
+var drinker = "False";                         // Created A Drinker Variables (Yes or No Question) (ESCAPE VARIABLE)
 var drunk = false;                             // Created A Drunk Variable (BOOLEAN VARIABLE)
-var drinkingAge = 21;                 // Created a Drinking Age Variable (NUMBER VARIABLE)
+var drinkingAge = 21;                          // Created a Drinking Age Variable (NUMBER VARIABLE)
 
 // OUTPUTS
 console.log("My name is " + myName + "."); // Tell the console who I am
@@ -18,22 +18,24 @@ console.log("My age is " + myAge + ".");   // Tell the console my age
 console.log("It is " + drinker + " that I am a drinker."); // Tell the console if I am a drinker
 console.log("It is " + drunk + " that I am drunk."); // Tell the console if you're drunk
 
+// TEST VARIABLE
 validAge = myAge - drinkingAge; // Do an operation to see if the user is old enough to drink.
 
-if(validAge >= 21) {
+// CONDITIONS TO RUN IF TRUE OR FALSE
+if(validAge >= 21) { // Test to see if the user is above 21
     alert("It looks like you can drink!."); // Let the user know that they can drink.
     console.log("The user can drink, for they are " + myAge + " years old."); // Log that into the console.
 
-    ready = confirm("Are you ready to go? (OK = Yes, CANCEL = No");
+    ready = confirm("Are you ready to go? (OK = Yes, CANCEL = No"); // Ask the user if they wish to continue or not. If not, exit.
 
-    if (ready === true) {
+    if (ready === true) { // Run if the user is ready.
 
         // Code to run if the condition "READY" is true
         alert("Looks like you're ready to begin!"); // Alert the user
         money = prompt("Please enter how much money you have:\nOnly use numbers!"); // Create a money variable
         console.log(myName + "has $" + money + "."); // Tell the console how much money I have
 
-        if (money >= 10) {
+        if (money >= 10) { // Code to run if the user has more than $10
             alert("Great! It looks like you have enough money."); // Alert the user that they have enough money
             drinkCost = prompt("Please type in how much drinks cost: \nOnly use numbers!"); // Create a Drink Cost Variable
             console.log("Drinks cost $" + drinkCost + " a piece."); // Tell the user how much drinks cost
@@ -63,3 +65,5 @@ if(validAge >= 21) {
     alert("It appears, " + myName + " that you're not old enough to drink!\nYou must be "+drinkingAge+" in order to drink. You're only "+myAge+".\nCome back in "+yearsUntil+" years."); // Alert the user that they cannot drink and when to come back.
     console.log("The user cannot drink. They'll need to come back in "+yearsUntil+" years."); // Log that into the console.
 }
+
+// END SCRIPT
