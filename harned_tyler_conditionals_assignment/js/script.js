5c8c569c8a29766d9f32a9bf9aefd409b69c9477
Tyler Harned <trharned@fullsail.edu>
2015-07-23 21:43:13 -0500

    Variables Created
    Created the variables and added the comments.

--- harned_tyler_conditionals_assignment/js/script.js
+++ harned_tyler_conditionals_assignment/js/script.js
@@ -4,3 +4,10 @@
  Conditionals Assignment
  "Bar Time!"
  */
+
+// THE VARIABLES
+var myName = prompt("Please Enter Your Name:"); // Created A Name Variable
+var myAge = prompt("Please Enter Your Age: "); // Created A Age Variable
+var drinker = "yes"/"no";                      // Created A Drinker Variables (Yes or No Question)
+var drunk = false;                             // Created A Drunk Variable
+
