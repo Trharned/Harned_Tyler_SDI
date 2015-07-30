0dfd737e220ac6ea3d3690c9156fb9a7ba275216
Tyler Harned <trharned@fullsail.edu>
2015-07-30 00:06:09 -0500

    Created Basic File Structures
    Created the basic file structures for the functions assignment. This
    includes the following:
     javascript folder/file
     index.html file
     Basic js file structure
       name, date, class, etc.
       tested to make sure worked
       commented out structure

---
+++ Harned_Tyler_Functions_Assignment/js/script.js
@@ -0,0 +1,27 @@
+/*
+    Tyler Harned
+    Full Sail University
+    Scalable Data Infrastructures #01
+    Functions Assignment
+    July 29th, 2015
+ */
+
+// alert("The JavaScript file works!");   // TEST TO SEE IF THE JAVASCRIPT FILE LOADED CORRECTLY
+
+// Create a random number generator that calculates either Powerball numbers or Florida Lottery numbers
+// Must contain BOTH Powerball and Florida generators
+// Must contain at least 
+
+//     ***     ***     ***     ***     *** //          VARIABLES           // ***     ***     ***     ***     ***     //
+
+
+
+
+
+//     ***     ***     ***     ***     *** //          FUNCTIONS           // ***     ***     ***     ***     ***     //
+
+
+
+
+
+//     ***     ***     ***     ***     *** //          MAIN CODE           // ***     ***     ***     ***     ***     //
